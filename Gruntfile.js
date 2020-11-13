68a6827bdd8c917f438ae2ae6bba32b98837acec
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2020-11-13 16:38:52 +0300

    Several improvements and bug fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,6 @@ module.exports = function(grunt) {
             distFolder: './dist'
         },
 
-
         // compiles handlebars template to html
         'handlebarslayouts': {
             templates: {
